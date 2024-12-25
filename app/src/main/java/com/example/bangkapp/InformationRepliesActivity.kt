dc050d4e6467a6b224e2c0488487eac25ad3c73a
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-12-25 15:03:10 +0700

    fix: replies activity

--- app/src/main/java/com/example/bangkapp/InformationRepliesActivity.kt
+++ app/src/main/java/com/example/bangkapp/InformationRepliesActivity.kt
@@ -1,12 +1,21 @@
 package com.example.bangkapp
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.marginBottom
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bangkapp.databinding.ActivityInformationDetailBinding
 import retrofit2.Call
@@ -48,12 +57,29 @@ class InformationRepliesActivity : AppCompatActivity() {
             if (username.isNotEmpty() && comment.isNotEmpty()) {
                 val informationReplies = InformationReplies(information?.id, username, "email", comment)
                 addInformationReplies(informationReplies)
+                binding.inputUsername.text.clear()
+                binding.inputComment.text.clear()
+                hideKeyboard()
             }
         }
 
         getInformationReplies(information?.id ?: 0)
 
+        binding.root.setOnApplyWindowInsetsListener { _, windowInsets ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                window.setDecorFitsSystemWindows(true)
+            }
+            windowInsets
+        }
+    }
 
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     fun getInformationReplies(id:Int){
